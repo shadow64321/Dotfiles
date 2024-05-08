6169fa2d8b299757684db76b9a31270df242299f
Elephants_Like_Crayons_Too <73987620+shadow64321@users.noreply.github.com>
2024-05-09 00:53:14 +1000

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+    alias music='musikcube'
+    alias fishy='asciiquarium'
+    alias rfishy='asciiquarium | lolcat'
+    alias uwu='uwufetch'
 end
