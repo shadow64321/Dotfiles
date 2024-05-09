c7e62b46e8c0940c649834c890eebba2b9056e1c
Elephants_Like_Crayons_Too <73987620+shadow64321@users.noreply.github.com>
2024-05-09 17:50:54 +1000

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+    alias music='musikcube'
+    alias fishy='asciiquarium'
+    alias rfishy='asciiquarium | lolcat'
+    alias uwu='uwufetch'
+    alias sysinf='btop'
+    alias ascii='ascii-image-converter'
+    alias update='sudo nixos-rebuild switch'
+end
