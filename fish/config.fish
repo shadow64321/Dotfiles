294af0f19a6f95d569a62a98f674d6cd95151e67
Elephants_Like_Crayons_Too <73987620+shadow64321@users.noreply.github.com>
2024-05-09 17:51:54 +1000

    Delete fish/config.fish

--- fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    starship init fish | source
-    alias music='musikcube'
-    alias fishy='asciiquarium'
-    alias rfishy='asciiquarium | lolcat'
-    alias uwu='uwufetch'
-end
