95a01af2e5558bd3b9ddd9128ad28e9f41af5fd4
Elephants_Like_Crayons_Too <73987620+shadow64321@users.noreply.github.com>
2024-05-13 00:12:38 +1000

    Delete bspwm/launch.sh

--- bspwm/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Autostart
-pgrep -x sxhkd > /dev/null || sxhkd &
-pkill picom &
-picom --experimental-backends &
-feh --bg-fill .config/hypr/Wallpapers/Nezuko.jpg &
-# ~/.config/polybar/launch.sh
-polybar
