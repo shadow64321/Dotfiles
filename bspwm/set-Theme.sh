e79d3df7d0f1be1b3518efb46b38a0bca2f31f46
Elephants_Like_Crayons_Too <73987620+shadow64321@users.noreply.github.com>
2024-05-13 20:59:45 +1000

    Add files via upload

--- bspwm/set-Theme.sh
+++ bspwm/set-Theme.sh
@@ -4,8 +4,9 @@
 # Random Wallpaper
 dirpapers='ls ~/.config/hypr/Wallpapers/*.jpg -1 | wc -l' &
 paperseleted=$((1 + RANDOM % $dirpapers))
-feh --bg-fill ~/.config/hypr/Wallpapers/$paperselected'.jpg' &
+feh --bg-fill ~/.config/hypr/Wallpapers/$paperselected &
 bash ~/.config/polybar/scripts/pywal.sh ~/.config/hypr/Wallpapers/$paperselected &
+betterlockscreen -u ~/.config/hypr/wallpapers/$paperselected &
 
 # Solid Wallpaper - Get on my level hoe
 #feh --bg-fill ~/.config/hypr/Wallpapers/'13.jpg' &
